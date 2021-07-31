397f934d302f5b192b0656efb930834867025816
Jhonnier Sanchez <jhonnier.sanchez@gmail.com>
2021-07-31 02:28:04 +0200

    ensayo 1

---
+++ docs/scripts.7465038aa8de93f97b6c.js
@@ -0,0 +1,1 @@
+function passwordShow(){if(document.getElementById("password").value.length>0){const t=document.querySelector("#passwordShow"),e="password"===document.querySelector("#password").getAttribute("type")?"text":"password";t.textContent="password"===e?"Mostrar":"Ocultar",document.querySelector("#password").setAttribute("type",e)}}
